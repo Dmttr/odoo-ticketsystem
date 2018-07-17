85a3f29281086ee8b915b14796b7971308cda4cd
DmytroHutkin <dmytro.hutkin@gmail.com>
2018-07-17 17:16:16 +0200

    moved login data to properties

--- src/main/java/de/odoo/ticketsystem/OdooAccount.java
+++ src/main/java/de/odoo/ticketsystem/OdooAccount.java
@@ -3,6 +3,7 @@ package de.odoo.ticketsystem;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,17 +16,23 @@ import static java.util.Collections.emptyMap;
 @RestController
 public class OdooAccount {
 
-    private final String db = "adesso1";
-    private final String username = "dmytro.hutkin@adesso.de";
-    private final String password = "123d321";
+    @Value("${odoo.url}")
+    private String url;
+    @Value("${odoo.db}")
+    private String db ;
+    @Value("${odoo.username}")
+    private String username;
+    @Value("${odoo.password}")
+    private String password;
+    private XmlRpcClient client;
 
     @RequestMapping("")
-    public Object authenticate() throws MalformedURLException {
-        String url = "https://adesso1.odoo.com";
-        final XmlRpcClient client = new XmlRpcClient();
+    public String authenticate() throws MalformedURLException {
+        client = new XmlRpcClient();
         final XmlRpcClientConfigImpl common_config = new XmlRpcClientConfigImpl();
         common_config.setServerURL(new URL(String.format("%s/xmlrpc/2/common", url)));
         try {
+
             client.execute(
                     common_config, "authenticate", asList(
                             db, username, password, emptyMap()));
