1a36dfc11bb2fae965bdd7b0331d50b1fd7f8660
DmytroHutkin <dmytro.hutkin@gmail.com>
2018-07-18 15:21:18 +0200

    extended with update and search-read methods

--- src/main/java/de/odoo/ticketsystem/OdooAccount.java
+++ src/main/java/de/odoo/ticketsystem/OdooAccount.java
@@ -5,12 +5,15 @@ import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.*;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 
 @RestController
@@ -25,6 +28,9 @@ public class OdooAccount {
     @Value("${odoo.password}")
     private String password;
     private XmlRpcClient client;
+    private XmlRpcClient models;
+    private boolean loggedIn = false;
+    private int uid;
 
     @RequestMapping("")
     public String authenticate() throws MalformedURLException {
@@ -33,13 +39,100 @@ public class OdooAccount {
         common_config.setServerURL(new URL(String.format("%s/xmlrpc/2/common", url)));
         try {
 
-            client.execute(
+            uid = (int)client.execute(
                     common_config, "authenticate", asList(
                             db, username, password, emptyMap()));
+            loggedIn = true;
             return "Login Successful!";
         } catch (XmlRpcException e) {
             e.printStackTrace();
             return e.getMessage();
         }
+    }
+
+    @RequestMapping("create")
+    public String createTicket() throws MalformedURLException {
+        models = new XmlRpcClient() {{
+            setConfig(new XmlRpcClientConfigImpl() {{
+                setServerURL(new URL(String.format("%s/xmlrpc/2/object", url)));
+            }});
+        }};
+        try {
+            int id = (Integer)models.execute("execute_kw", asList(
+                    db, uid, password,
+                    "helpdesk.ticket", "create",
+                    asList(new HashMap() {{ put("name", "Test ticket"); }})
+            ));
+            return "" + id;
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+    @RequestMapping("search-read")
+    public List readTicket(@RequestParam(value = "name", defaultValue = "Test ticket")String name)
+            throws MalformedURLException {
+        models = new XmlRpcClient() {{
+            setConfig(new XmlRpcClientConfigImpl() {{
+                setServerURL(new URL(String.format("%s/xmlrpc/2/object", url)));
+            }});
+        }};
+        try {
+            return asList((Object[])models.execute("execute_kw", asList(
+                    db, uid, password,
+                    "helpdesk.ticket", "search_read",
+                    asList(asList(
+                            asList("name", "=", name))),
+                    new HashMap() {{
+                        put("fields", asList("name", "country_id", "im_status", "comment", "color"));
+                        put("limit", 5);
+                    }}
+            )));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+            return asList(e.getMessage());
+        }
+    }
+    @RequestMapping("getfields")
+    public Map<String, Map<String, Object>> getFields() throws MalformedURLException {
+        models = new XmlRpcClient() {{
+            setConfig(new XmlRpcClientConfigImpl() {{
+                setServerURL(new URL(String.format("%s/xmlrpc/2/object", url)));
+            }});
+        }};
+        try {
+            return (Map<String, Map<String, Object>>)models.execute("execute_kw", asList(
+                    db, uid, password,
+                    "res.partner", "fields_get",
+                    emptyList(),
+                    new HashMap() {{
+                        put("attributes", asList("string", "help", "type"));
+                    }}
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    @RequestMapping("update")
+    public String updateRecord(@RequestParam(value = "id", defaultValue = "1")int id) throws MalformedURLException {
+        models = new XmlRpcClient() {{
+            setConfig(new XmlRpcClientConfigImpl() {{
+                setServerURL(new URL(String.format("%s/xmlrpc/2/object", url)));
+            }});
+        }};
+        try {
+            models.execute("execute_kw", asList(
+                    db, uid, password,
+                    "res.partner", "write",
+                    asList(
+                            asList(id),
+                            new HashMap() {{ put("color", 3); }}
+                    )
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
